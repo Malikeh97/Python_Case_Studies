41a7541b69a9dd8a6afec054f770cf2223044859
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-08 21:27:40 -0400

    count_word function implemented

---
+++ language_processing/language_jpo.py
@@ -0,0 +1,17 @@
+text = "This is my test text. We're keeping this text short to keep things manageable"
+
+def count_word(text):
+    word_counts = {}
+    text = text.lower()
+    skips = [".", ",", ";", ":", '"', "'"]
+    for ch in skips:
+        text = text.replace(ch, "")
+
+    for word in text.split(" "):
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    return word_counts
+print(count_word(text))
