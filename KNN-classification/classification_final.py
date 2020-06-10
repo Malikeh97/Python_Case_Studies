43ecb30595ba58131f44599a3c11834d2ef8cec6
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-10 15:23:01 -0400

    majority voting implemented

--- KNN-classification/classification_final.py
+++ KNN-classification/classification_final.py
@@ -1,7 +1,28 @@
 import numpy as np
+from collections import Counter
+import random
+
 p1 = np.array([1,1])
 p2 = np.array([4,4])
 
 def distance(p1, p2):
     """Finds the distance between points p1 and p2"""
     return  np.sqrt(np.sum(np.power(p1 - p2, 2)))
+
+def majority_vote(votes):
+    vote_counts = {}
+    for vote in votes:
+        if vote in vote_counts:
+            vote_counts[vote] += 1
+        else:
+            vote_counts[vote] = 1
+    max_count = max(vote_counts.values())
+    winners = []
+    for vote, count in vote_counts.items():
+        if count == max_count:
+            winners.append(vote)
+    return random.choice(winners)
+
+
+votes = [1,2,3,1,2,3,1,2,3,3,3,3,3]
+print(majority_vote(votes))
