d23dd49dc2c68d5c5a852bfab1f5375a9257064c
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-10 18:57:50 -0400

    knn neighbor function implemented

--- KNN-classification/classification_final.py
+++ KNN-classification/classification_final.py
@@ -2,6 +2,7 @@ import numpy as np
 from collections import Counter
 import random
 import scipy.stats as ss
+import matplotlib.pyplot as plt
 
 p1 = np.array([1,1])
 p2 = np.array([4,4])
@@ -34,10 +35,26 @@ def majority_vote_short(votes):
     mode, count = ss.mstats.mode(votes)
     return mode
 
+def find_nearest_neighbors(p, points, k=5):
+    distances = np.zeros(points.shape[0])
+    for i in range(len(distances)):
+        distances[i] = distance(p, points[i])
+    ind = np.argsort(distances)
+    return(ind)
+
+def knn_predict(p , points, outcomes, k):
+    ind = find_nearest_neighbors(p, points, k)
+    return majority_vote(outcomes[ind])
 
 
 
+points = np.array([[1,1], [1,2], [1,3], [2,1] ,[2,2] ,[2,3], [3,1], [3,2], [3,3]])
+p = np.array([2.5, 2])
+outcomes = np.array([0,0,0,0,1,1,1,1,1])
+print(knn_predict(p , points, outcomes, 2))
 
-votes = [1,2,3,1,2,3,1,2,3,3,3,3,3]
-print(majority_vote(votes))
-print(majority_vote_short(votes))
+
+plt.plot(points[:,0], points[:,1], "ro")
+plt.plot(p[0], p[1], "bo")
+plt.axis([0.5,3.5, 0.5, 3.5])
+plt.show()
