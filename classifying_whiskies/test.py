d1c99c3643d0c8a9cfc425f48480ce53e1d72ce8
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-13 16:47:51 -0400

    correlation exploration between flavors done

---
+++ classifying_whiskies/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+whisky = pd.read_csv("whiskies.txt")
+whisky["Regions"] = pd.read_csv("regions.txt")
+flavors = whisky.iloc[:, 2:14]
+corr_flavors = pd.DataFrame.corr(flavors)
+plt.figure(figsize = (10, 10))
+plt.pcolor(corr_flavors)
+plt.colorbar()
+plt.savefig("corr_flavors.pdf")
