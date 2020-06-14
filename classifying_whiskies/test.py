35a58e2a1462a7420ee5f3baf813fbbe74c27327
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-13 20:45:03 -0400

    clustering whiskies by profile done

--- classifying_whiskies/test.py
+++ classifying_whiskies/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster.bicluster import SpectralCoclustering
 
 whisky = pd.read_csv("whiskies.txt")
 whisky["Regions"] = pd.read_csv("regions.txt")
@@ -17,3 +18,9 @@ plt.pcolor(corr_whiskies)
 plt.axis("tight")
 plt.colorbar()
 plt.savefig("corr_whiskies.pdf")
+
+model = SpectralCoclustering(n_clusters=6, random_state=0)
+model.fit(corr_whiskies)
+print(np.sum(model.rows_, axis = 1)) # number of whiskies in each cluster
+print(np.sum(model.rows_, axis = 0)) # number of clusters for each observation
+print(model.row_labels_)
