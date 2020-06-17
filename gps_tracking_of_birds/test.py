988b575c35d7d3d3002a81189f1d1f67bd852c5c
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-16 20:59:24 -0400

    hist plotting optimized by pandas

--- gps_tracking_of_birds/test.py
+++ gps_tracking_of_birds/test.py
@@ -13,3 +13,8 @@ plt.hist(speed[~ind], bins = np.linspace(0, 30, 20), normed = True)
 plt.xlabel("2D speed (m/s)")
 plt.ylabel("Frequency")
 plt.savefig("speed_hist.pdf")
+
+#no need to deal with nans explicitly using pandas
+birddata.speed_2d.plot(kind = "hist", range = [0, 30])
+plt.xlabel("2D speed (m/s)")
+plt.savefig("pd_hist")
