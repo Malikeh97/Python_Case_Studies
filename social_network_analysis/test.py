aeebe404e628642b92bdb4f495fe60dc4fbc5f4f
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-17 16:22:33 -0400

    er graph plotted

--- social_network_analysis/test.py
+++ social_network_analysis/test.py
@@ -1,6 +1,19 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from scipy.stats import bernoulli
 
-G = nx.karate_club_graph()
-nx.draw(G, with_labels = "True", node_color="lightblue", edge_color="grey")
-plt.savefig("karate_graph.pdf")
+N = 20
+p = 0.2
+
+def er_graph(N, p):
+    """Generate an ER Graph."""
+    G = nx.Graph()
+    G.add_nodes_from(range(N))
+    for node1 in G.nodes():
+        for node2 in G.nodes():
+            if node1 < node2 and bernoulli.rvs(p=p):
+                    G.add_edge(node1, node2)
+    return G
+G  = er_graph(N, p)
+nx.draw(G, with_labels=True, node_color="pink", edge_color="lightblue")
+plt.savefig("er_graph.pdf")
