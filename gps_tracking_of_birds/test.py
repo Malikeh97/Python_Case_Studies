97903fd0950a5779ae95f0ae4bff97a052ac8068
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-16 20:55:33 -0400

    speed hist plotted completely

--- gps_tracking_of_birds/test.py
+++ gps_tracking_of_birds/test.py
@@ -8,17 +8,8 @@ x , y = birddata.longitude[ix], birddata.latitude[ix]
 bird_names = pd.unique(birddata.bird_name)
 speed = birddata.speed_2d[ix]
 ind = np.isnan(speed)
-plt.hist(speed[~ind])
-plt.savefig("hist.pdf")
-# plt.figure(figsize=(7,7))
-# for birdname in bird_names:
-#     ix = birddata.bird_name == birdname
-#     x , y = birddata.longitude[ix], birddata.latitude[ix]
-#     plt.plot(x, y, ".", label = birdname)
-# plt.xlabel("Longitude")
-# plt.ylabel("Latitude")
-# plt.legend(loc = "lower right")
-# plt.savefig("3traj.pdf")
-# plt.show()
-# plt.hist(speed[:10])
-# plt.show()
+plt.figure(figsize = (8,4))
+plt.hist(speed[~ind], bins = np.linspace(0, 30, 20), normed = True)
+plt.xlabel("2D speed (m/s)")
+plt.ylabel("Frequency")
+plt.savefig("speed_hist.pdf")
