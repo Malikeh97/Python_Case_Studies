65a00ed7305c3bc13e65bc943ae67649932ff631
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-16 20:33:22 -0400

    simple data visualization done

--- gps_tracking_of_birds/test.py
+++ gps_tracking_of_birds/test.py
@@ -5,7 +5,21 @@ import numpy as np
 birddata = pd.read_csv("bird_tracking.csv")
 ix = birddata.bird_name == 'Eric'
 x , y = birddata.longitude[ix], birddata.latitude[ix]
-print(x)
+# print(x)
+# plt.figure(figsize=(7,7))
+# plt.plot(x, y, ".")
+# plt.show()
+bird_names = pd.unique(birddata.bird_name)
+i = 1
 plt.figure(figsize=(7,7))
-plt.plot(x, y, ".")
+
+for birdname in bird_names:
+    ix = birddata.bird_name == birdname
+    x , y = birddata.longitude[ix], birddata.latitude[ix]
+    i += 1
+    plt.plot(x, y, ".", label = birdname)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.legend(loc = "lower right")
+plt.savefig("3traj.pdf")
 plt.show()
