28e81ccbc03139ead4432cb454cfa5d96b7f9fc8
Malikeh97 <malikehehghaghi@gmail.com>
2020-06-21 21:25:46 -0400

    multiple linear regresssion random dataset generated

---
+++ regression/multiple_linear_regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy.stats as ss
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import statsmodels.api as sm
+
+n = 500
+beta_0 = 5
+beta_1 = 2
+beta_2 = -1
+np.random.seed(1)
+x_1 = 10 * ss.uniform.rvs(size=n)
+x_2 = 10 * ss.uniform.rvs(size=n)
+y = beta_0 + beta_1 * x_1 + beta_2 * x_2 + ss.norm.rvs(loc=0, scale = 1, size = n)
+X = np.stack([x_1, x_2], axis=1) #axis = 1 to put x1 and x2 in columns
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:, 0], X[:, 1], y, c=y)
+ax.set_xlabel("$x_1$")
+ax.set_ylabel("$x_2$")
+ax.set_zlabel("$y$")
+plt.show()
--- regression/test.py
+++ regression/test.py
@@ -12,19 +12,6 @@ x = 10 * ss.uniform.rvs(size=n)
 y = beta_0 + beta_1 * x + ss.norm.rvs(loc=0, scale = 1, size = n)
 
 
-# plt.plot(x, y, ".", ms=5)
-
-# xx = np.array([0, 10])
-# plt.plot(xx, beta_0 + beta_1*xx)
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.show()
-# def compute_rss(y_estimate, y):
-#   return sum(np.power(y-y_estimate, 2))
-# def estimate_y(x, b_0, b_1):
-#   return b_0 + b_1 * x
-# rss = compute_rss(estimate_y(x, beta_0, beta_1), y)
-# print(rss)
 rss = []
 slopes = np.arange(-10, 15, 0.01)
 for slope in slopes:
@@ -34,11 +21,11 @@ for slope in slopes:
 ind_min = np.argmin(rss)
 print(slopes[ind_min])
 
-# plt.figure()
-# plt.plot(slopes, rss)
-# plt.xlabel("slope")
-# plt.ylabel("rss")
-# plt.show()
+plt.figure()
+plt.plot(slopes, rss)
+plt.xlabel("slope")
+plt.ylabel("rss")
+plt.show()
 
 #with no intercept
 mod = sm.OLS(y, x) #ordinary least squares
